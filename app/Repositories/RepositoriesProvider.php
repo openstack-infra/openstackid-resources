67f99afd1801041fa1c950fcf538ec7a07a24493
Sebastian Marcet <smarcet@gmail.com>
2015-09-09 18:14:28 -0300

    Summit Application API
    WIP - Added Summit API
    
    Change-Id: I5494db9414809a3ca5e278919d084e895b2ed05a

--- app/Repositories/RepositoriesProvider.php
+++ app/Repositories/RepositoriesProvider.php
@@ -1,49 +1,60 @@
 <?php namespace repositories;
-/**
-* Copyright 2015 OpenStack Foundation
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* http://www.apache.org/licenses/LICENSE-2.0
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-**/
 
-use Illuminate\Support\ServiceProvider;
+/**
+ * Copyright 2015 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\ServiceProvider;
 
 /**
-* Class RepositoriesProvider
-* @package repositories
-*/
+ * Class RepositoriesProvider
+ * @package repositories
+ */
 class RepositoriesProvider extends ServiceProvider
 {
-	protected $defer = false;
+    protected $defer = false;
 
-	public function boot()
-	{
-	}
+    public function boot()
+    {
+    }
 
-	public function register()
-	{
-		App::singleton(
-						'models\marketplace\IPublicCloudServiceRepository',
-						'repositories\marketplace\EloquentPublicCloudServiceRepository'
-		);
-		App::singleton(
-						'models\marketplace\IPrivateCloudServiceRepository',
-						'repositories\marketplace\EloquentPrivateCloudServiceRepository'
-		);
-		App::singleton(
-						'models\marketplace\IConsultantRepository',
-						'repositories\marketplace\EloquentConsultantRepository'
-		);
-		App::singleton(
-						'models\resource_server\IApiEndpointRepository',
-						'repositories\resource_server\EloquentApiEndpointRepository'
-		);
-	}
+    public function register()
+    {
+        App::singleton(
+            'models\marketplace\IPublicCloudServiceRepository',
+            'repositories\marketplace\EloquentPublicCloudServiceRepository'
+        );
+        App::singleton(
+            'models\marketplace\IPrivateCloudServiceRepository',
+            'repositories\marketplace\EloquentPrivateCloudServiceRepository'
+        );
+        App::singleton(
+            'models\marketplace\IConsultantRepository',
+            'repositories\marketplace\EloquentConsultantRepository'
+        );
+        App::singleton(
+            'models\resource_server\IApiEndpointRepository',
+            'repositories\resource_server\EloquentApiEndpointRepository'
+        );
+
+        App::singleton(
+            'models\summit\ISummitRepository',
+            'repositories\summit\EloquentSummitRepository'
+        );
+
+        App::singleton(
+            'models\summit\ISpeakerRepository',
+            'repositories\summit\EloquentSpeakerRepository'
+        );
+    }
 }
