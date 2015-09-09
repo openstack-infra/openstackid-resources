08a95f779654a8cc4552e93123885987d0986834
Sebastian Marcet <smarcet@gmail.com>
2015-09-09 18:14:28 -0300

    Summit Application API
    WIP - Added Summit API
    
    Change-Id: I5494db9414809a3ca5e278919d084e895b2ed05a

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,36 +2,35 @@
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
-class Kernel extends HttpKernel {
+class Kernel extends HttpKernel
+{
 
-	/**
-	 * The application's global HTTP middleware stack.
-	 *
-	 * @var array
-	 */
-	protected $middleware = [
-		'Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode',
-		'Illuminate\Cookie\Middleware\EncryptCookies',
-		'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
-		'Illuminate\Session\Middleware\StartSession',
-		'Illuminate\View\Middleware\ShareErrorsFromSession',
-		'App\Http\Middleware\VerifyCsrfToken',
-		'App\Http\Middleware\CORSMiddleware',
-		'App\Http\Middleware\SecurityHTTPHeadersWriterMiddleware',
-	];
+    /**
+     * The application's global HTTP middleware stack.
+     * @var array
+     */
+    protected $middleware = [
+        'Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode',
+        'Illuminate\Cookie\Middleware\EncryptCookies',
+        'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
+        'Illuminate\Session\Middleware\StartSession',
+        'Illuminate\View\Middleware\ShareErrorsFromSession',
+        'App\Http\Middleware\CORSMiddleware',
+        'App\Http\Middleware\SecurityHTTPHeadersWriterMiddleware',
+    ];
 
-	/**
-	 * The application's route middleware.
-	 *
-	 * @var array
-	 */
-	protected $routeMiddleware = [
-		'auth'             => 'App\Http\Middleware\Authenticate',
-		'auth.basic'       => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
-		'guest'            => 'App\Http\Middleware\RedirectIfAuthenticated',
-		'oauth2.protected' => 'App\Http\Middleware\OAuth2BearerAccessTokenRequestValidator',
-		'rate.limit'       => 'App\Http\Middleware\RateLimitMiddleware',
-		'etags'            => 'App\Http\Middleware\ETagsMiddleware',
-	];
+    /**
+     * The application's route middleware.
+     * @var array
+     */
+    protected $routeMiddleware = [
+        'auth'             => 'App\Http\Middleware\Authenticate',
+        'auth.basic'       => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
+        'guest'            => 'App\Http\Middleware\RedirectIfAuthenticated',
+        'oauth2.protected' => 'App\Http\Middleware\OAuth2BearerAccessTokenRequestValidator',
+        'rate.limit'       => 'App\Http\Middleware\RateLimitMiddleware',
+        'etags'            => 'App\Http\Middleware\ETagsMiddleware',
+        'cache'            => 'App\Http\Middleware\CacheMiddleware',
+    ];
 
 }
