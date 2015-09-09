615e04a9484ef8e7fb9a12ebdcb525310b0d5d0e
Sebastian Marcet <smarcet@gmail.com>
2015-09-09 18:14:28 -0300

    Summit Application API
    WIP - Added Summit API
    
    Change-Id: I5494db9414809a3ca5e278919d084e895b2ed05a

---
+++ app/Http/Middleware/CacheMiddleware.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Copyright 2015 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Config;
+use Illuminate\Contracts\Routing\Middleware;
+use Illuminate\Http\JsonResponse;
+use libs\utils\ICacheService;
+use Log;
+use models\oauth2\IResourceServerContext;
+
+/**
+ * Class CacheMiddleware
+ * @package App\Http\Middleware
+ */
+class CacheMiddleware implements Middleware
+{
+    /**
+     * @var ICacheService
+     */
+    private $cache_service;
+
+    /**
+     * @var IResourceServerContext
+     */
+    private $context;
+
+    public function __construct(IResourceServerContext $context, ICacheService $cache_service)
+    {
+        $this->context = $context;
+        $this->cache_service = $cache_service;
+    }
+
+
+    /**
+     * Handle an incoming request.
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->getMethod() !== 'GET')
+        {
+            // shortcircuit
+            return $next($request);
+        }
+
+        $key   = $request->getPathInfo();
+        $query = $request->getQueryString();
+
+        if(!empty($query))
+        {
+            $query = explode('&', $query);
+            foreach($query as $q)
+            {
+                $q = explode('=',$q);
+                if(strtolower($q[0]) === 'access_token'|| strtolower($q[0]) === 'token_type' ) continue;
+                $key .= ".".implode("=",$q);
+            }
+        }
+
+        $cache_lifetime = intval(Config::get('server.response_cache_lifetime', 300));
+
+        if (str_contains($request->getPathInfo(), '/me'))
+        {
+            $key .= ':' . $this->context->getCurrentUserExternalId();
+        }
+
+        $data = $this->cache_service->getSingleValue($key);
+        $time = $this->cache_service->getSingleValue($key.".generated");
+
+        if (empty($data) || empty($time))
+        {
+            Log::debug(sprintf("cache value not found for key %s , getting from api...", $key));
+            // normal flow ...
+            $response = $next($request);
+            if ($response instanceof JsonResponse && $response->getStatusCode() === 200)
+            {
+                // and if its json, store it on cache ...
+                $data = $response->getData(true);
+                $this->cache_service->setSingleValue($key, json_encode($data), $cache_lifetime);
+                $this->cache_service->setSingleValue($key.".generated", time(), $cache_lifetime);
+            }
+        } else {
+            // cache hit ...
+            Log::debug(sprintf("cache hit for %s ...", $key));
+            $response = new JsonResponse(json_decode($data, true), 200, array
+                (
+                    'content-type'       => 'application/json',
+                )
+            );
+            $response->header("xcontent-timestamp", $time);
+        }
+
+        return $response;
+    }
+}
--- app/Http/Middleware/ETagsMiddleware.php
+++ app/Http/Middleware/ETagsMiddleware.php
@@ -1,16 +1,17 @@
 <?php namespace App\Http\Middleware;
+
 /**
-* Copyright 2015 OpenStack Foundation
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* http://www.apache.org/licenses/LICENSE-2.0
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-**/
+ * Copyright 2015 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 
 use Closure;
 use Illuminate\Contracts\Routing\Middleware;
@@ -19,26 +20,24 @@ class ETagsMiddleware implements Middleware
 {
 
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @param  \Closure $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-		$response = $next($request);
-		if ($response->getStatusCode() === 200)
-		{
-			$etag = md5($response->getContent());
-			$requestETag = str_replace('"', '', $request->getETags());
-			if ($requestETag && $requestETag[0] == $etag)
-			{
-				$response->setNotModified();
-			}
-			$response->setEtag($etag);
-		}
-		return $response;
-	}
+    /**
+     * Handle an incoming request.
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        if ($response->getStatusCode() === 200 && $request->getMethod() === 'GET') {
+            $etag = md5($response->getContent());
+            $requestETag = str_replace('"', '', $request->getETags());
+            if ($requestETag && $requestETag[0] == $etag) {
+                $response->setNotModified();
+            }
+            $response->setEtag($etag);
+        }
+
+        return $response;
+    }
 }
