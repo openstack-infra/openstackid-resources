cff35cbe13b2776e2b51524ba56c694b93d00f89
Sebastian Marcet <smarcet@gmail.com>
2015-09-09 18:14:28 -0300

    Summit Application API
    WIP - Added Summit API
    
    Change-Id: I5494db9414809a3ca5e278919d084e895b2ed05a

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -1,99 +1,124 @@
 <?php namespace App\Http\Controllers;
-/**
-* Copyright 2015 OpenStack Foundation
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* http://www.apache.org/licenses/LICENSE-2.0
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-**/
 
+/**
+ * Copyright 2015 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\Input;
+use Exception;
 
 /**
-* Class JsonController
-* @package App\Http\Controllers
-*/
+ * Class JsonController
+ * @package App\Http\Controllers
+ */
 abstract class JsonController extends Controller
 {
 
-	protected $log_service;
+    protected $log_service;
 
-	public function __construct()
-	{
-	}
+    public function __construct()
+    {
+    }
 
-	protected function error500(Exception $ex)
-	{
-		Log::error($ex);
-		return Response::json(array('message' => 'server error'), 500);
-	}
+    protected function error500(Exception $ex)
+    {
+        Log::error($ex);
 
-	protected function created($data = 'ok')
-	{
-		$res = Response::json($data, 201);
-		//jsonp
-		if (Input::has('callback'))
-		{
-			$res->setCallback(Input::get('callback'));
-		}
-		return $res;
-	}
+        return Response::json(array('message' => 'server error'), 500);
+    }
 
-	protected function deleted($data = 'ok')
-	{
-		$res =  Response::json($data, 204);
-		//jsonp
-		if (Input::has('callback'))
-		{
-			$res->setCallback(Input::get('callback'));
-		}
-		return $res;
-	}
+    protected function created($data = 'ok')
+    {
+        $res = Response::json($data, 201);
+        //jsonp
+        if (Input::has('callback')) {
+            $res->setCallback(Input::get('callback'));
+        }
 
-	protected function ok($data = 'ok')
-	{
-		$res = Response::json($data, 200);
-		//jsonp
-		if (Input::has('callback'))
-		{
-			$res->setCallback(Input::get('callback'));
-		}
-		return $res;
-	}
+        return $res;
+    }
 
-	protected function error400($data)
-	{
-		return Response::json($data, 400);
-	}
+    protected function deleted($data = 'ok')
+    {
+        $res = Response::json($data, 204);
+        //jsonp
+        if (Input::has('callback')) {
+            $res->setCallback(Input::get('callback'));
+        }
 
-	protected function error404($data = array('message' => 'Entity Not Found'))
-	{
-		return Response::json($data, 404);
-	}
+        return $res;
+    }
 
-	/**
-	 *  {
-	"message": "Validation Failed",
-	"errors": [
-	{
-	"resource": "Issue",
-	"field": "title",
-	"code": "missing_field"
-	}
-	]
-	}
-	 * @param $messages
-	 * @return mixed
-	 */
-	protected function error412($messages)
-	{
-		return Response::json(array('message' => 'Validation Failed', 'errors' => $messages), 412);
-	}
+    protected function updated($data = 'ok')
+    {
+        $res = Response::json($data, 204);
+        //jsonp
+        if (Input::has('callback')) {
+            $res->setCallback(Input::get('callback'));
+        }
+
+        return $res;
+    }
+
+
+    protected function ok($data = 'ok')
+    {
+        $res = Response::json($data, 200);
+        //jsonp
+        if (Input::has('callback')) {
+            $res->setCallback(Input::get('callback'));
+        }
+
+        return $res;
+    }
+
+    protected function error400($data)
+    {
+        return Response::json($data, 400);
+    }
+
+    protected function error404($data = array('message' => 'Entity Not Found'))
+    {
+        return Response::json($data, 404);
+    }
+
+    protected function error403($data = array('message' => 'Forbidden'))
+    {
+        return Response::json($data, 403);
+    }
+
+    protected function error401($data = array('message' => 'You don\'t have access to this item through the API.'))
+    {
+        return Response::json($data, 401);
+    }
+
+    /**
+     *  {
+     * "message": "Validation Failed",
+     * "errors": [
+     * {
+     * "resource": "Issue",
+     * "field": "title",
+     * "code": "missing_field"
+     * }
+     * ]
+     * }
+     * @param $messages
+     * @return mixed
+     */
+    protected function error412($messages)
+    {
+        return Response::json(array('message' => 'Validation Failed', 'errors' => $messages), 412);
+    }
 }
