c56d7e0a0181e66cafd2333835c56c4bd7b86b73
Sebastian Marcet <smarcet@gmail.com>
2015-09-09 18:14:28 -0300

    Summit Application API
    WIP - Added Summit API
    
    Change-Id: I5494db9414809a3ca5e278919d084e895b2ed05a

---
+++ app/Models/summit/SummitEventFeedback.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Copyright 2015 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+namespace models\summit;
+
+use libs\utils\JsonUtils;
+use models\utils\SilverstripeBaseModel;
+use models\main\Member;
+/**
+ * Class SummitEventFeedback
+ * @package models\summit
+ */
+class SummitEventFeedback extends SilverstripeBaseModel
+{
+    protected $table = 'SummitEventFeedback';
+
+    protected $mtiClassType = 'concrete';
+
+    protected $array_mappings = array
+    (
+        'ID'      => 'id:json_int',
+        'Rate'    => 'rate:json_int',
+        'Note'    => 'note:json_string',
+        'OwnerID' => 'owner_id:json_int',
+        'EventID' => 'event_id:json_int',
+        'Created' => 'created_date:datetime_epoch',
+    );
+
+    /**
+     * @return Member
+     */
+    public function owner()
+    {
+        return $this->hasOne('models\main\Member', 'ID', 'OwnerID')->first();
+    }
+
+    /**
+     * @return SummitEvent
+     */
+    public function event()
+    {
+        return $this->hasOne('models\summit\SummitEvent', 'ID', 'EventID')->first();
+    }
+
+    /**
+     * @param bool|false $show_attendee_info
+     */
+    public function toArray($show_attendee_info = false)
+    {
+        $data = parent::toArray();
+        $member_id = $data['owner_id'];
+
+        $attendee = SummitAttendee::where('MemberID', '=', $member_id)->first();
+        if(!is_null($attendee))
+        {
+            unset($data['owner_id']);
+            $member = $attendee->member();
+            $data['owner'] = array
+            (
+                'id'         => intval($attendee->ID),
+                'first_name' => JsonUtils::toJsonString($member->FirstName),
+                'last_name'  => JsonUtils::toJsonString($member->Surname)
+            );
+        }
+    }
+}
