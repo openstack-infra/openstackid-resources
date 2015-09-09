4933e83204e7292c9ff312e7e0870e2c96a3aece
Sebastian Marcet <smarcet@gmail.com>
2015-09-09 18:14:28 -0300

    Summit Application API
    WIP - Added Summit API
    
    Change-Id: I5494db9414809a3ca5e278919d084e895b2ed05a

---
+++ app/Http/Controllers/apis/protected/summit/strategies/CheckMyOwnAttendeeStrategy.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright 2015 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+namespace App\Http\Controllers;
+
+use models\oauth2\IResourceServerContext;
+use models\summit\Summit;
+use models\summit\SummitAttendee;
+
+/**
+ * Class CheckMyOwnAttendeeStrategy
+ * @package App\Http\Controllers
+ */
+final class CheckMyOwnAttendeeStrategy extends CheckMeAttendeeStrategy implements ICheckAttendeeStrategy
+{
+
+    /**
+     * @param int $attendee_id
+     * @param Summit $summit
+     * @return null|SummitAttendee
+     * @throws \HTTP401UnauthorizedException
+     */
+    public function check($attendee_id, Summit $summit)
+    {
+        $attendee = parent::check($attendee_id, $summit);
+        if(!$attendee) return false;
+        $attendee_member_id = intval($attendee->member()->ID);
+        $member_id          = $this->resource_server_context->getCurrentUserExternalId();
+        if(is_null($member_id) || ($attendee_member_id !== $member_id)) throw new \HTTP401UnauthorizedException;
+        return $attendee;
+    }
+}
--- app/Http/Middleware/RateLimitMiddleware.php
+++ app/Http/Middleware/RateLimitMiddleware.php
@@ -1,106 +1,101 @@
 <?php namespace App\Http\Middleware;
+
 /**
-* Copyright 2015 OpenStack Foundation
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* http://www.apache.org/licenses/LICENSE-2.0
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-**/
+ * Copyright 2015 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 
 use Closure;
-use libs\utils\ICacheService;
-use models\resource_server\IApiEndpointRepository;
 use Illuminate\Contracts\Routing\Middleware;
 use Illuminate\Support\Facades\Response;
+use libs\utils\ICacheService;
 use libs\utils\RequestUtils;
+use models\resource_server\IApiEndpointRepository;
 
 /**
-* Class RateLimitMiddleware
-* @package App\Http\Middleware
-*/
+ * Class RateLimitMiddleware
+ * @package App\Http\Middleware
+ */
 final class RateLimitMiddleware implements Middleware
 {
 
-	/**
-	 * @var IApiEndpointRepository
-	 */
-	private $endpoint_repository;
+    /**
+     * @var IApiEndpointRepository
+     */
+    private $endpoint_repository;
 
-	/**
-	 * @var ICacheService
-	 */
-	private $cache_service;
+    /**
+     * @var ICacheService
+     */
+    private $cache_service;
 
-	/**
-	 * @param IApiEndpointRepository $endpoint_repository
-	 * @param ICacheService $cache_service
-	 */
-	public function __construct(IApiEndpointRepository $endpoint_repository, ICacheService $cache_service)
-	{
-		$this->endpoint_repository = $endpoint_repository;
-		$this->cache_service       = $cache_service;
-	}
+    /**
+     * @param IApiEndpointRepository $endpoint_repository
+     * @param ICacheService $cache_service
+     */
+    public function __construct(IApiEndpointRepository $endpoint_repository, ICacheService $cache_service)
+    {
+        $this->endpoint_repository = $endpoint_repository;
+        $this->cache_service = $cache_service;
+    }
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @param  \Closure $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-		$response = $next($request);
-		// if response was not changed then short circuit ...
-		if ($response->getStatusCode() === 304)
-		{
-			return $response;
-		}
+    /**
+     * Handle an incoming request.
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        // if response was not changed then short circuit ...
+        if ($response->getStatusCode() === 304) {
+            return $response;
+        }
 
-		$url  = $request->getRequestUri();
+        $url = $request->getRequestUri();
 
-		try
-		{
-			$route    = RequestUtils::getCurrentRoutePath($request);
-			$method   = $request->getMethod();
-			$endpoint = $this->endpoint_repository->getApiEndpointByUrlAndMethod($route, $method);
+        try {
+            $route = RequestUtils::getCurrentRoutePath($request);
+            $method = $request->getMethod();
+            $endpoint = $this->endpoint_repository->getApiEndpointByUrlAndMethod($route, $method);
 
-			if (!is_null($endpoint->rate_limit) && ($requestsPerHour = (int)$endpoint->rate_limit) > 0)
-			{
-				//do rate limit checking
-				$key = sprintf('rate.limit.%s_%s_%s', $url, $method, $request->getClientIp());
-				// Add if doesn't exist
-				// Remember for 1 hour
-				$this->cache_service->addSingleValue($key, 0, 3600);
-				// Add to count
-				$count = $this->cache_service->incCounter($key);
-				if ( $count > $requestsPerHour )
-				{
-					// Short-circuit response - we're ignoring
-					$response = Response::json(array(
-						'message' => "You have triggered an abuse detection mechanism and have been temporarily blocked.
-						 Please retry your request again later."), 403);
-					$ttl      = (int) $this->cache_service->ttl($key);
-					$response->headers->set('X-RateLimit-Reset', $ttl, false);
-				}
-				$response->headers->set('X-Ratelimit-Limit', $requestsPerHour, false);
-				$remaining = $requestsPerHour-(int)$count;
-				if ($remaining < 0)
-				{
-					$remaining = 0;
-				}
-				$response->headers->set('X-Ratelimit-Remaining', $remaining, false);
-			}
-		}
-		catch (Exception $ex)
-		{
-			Log::error($ex);
-		}
-		return $response;
-	}
+            if (!is_null($endpoint->rate_limit) && ($requestsPerHour = (int)$endpoint->rate_limit) > 0) {
+                //do rate limit checking
+                $key = sprintf('rate.limit.%s_%s_%s', $url, $method, $request->getClientIp());
+                // Add if doesn't exist
+                // Remember for 1 hour
+                $this->cache_service->addSingleValue($key, 0, 3600);
+                // Add to count
+                $count = $this->cache_service->incCounter($key);
+                if ($count > $requestsPerHour) {
+                    // Short-circuit response - we're ignoring
+                    $response = Response::json(array(
+                        'message' => "You have triggered an abuse detection mechanism and have been temporarily blocked.
+                         Please retry your request again later."
+                    ), 403);
+                    $ttl = (int)$this->cache_service->ttl($key);
+                    $response->headers->set('X-RateLimit-Reset', $ttl, false);
+                }
+                $response->headers->set('X-Ratelimit-Limit', $requestsPerHour, false);
+                $remaining = $requestsPerHour - (int)$count;
+                if ($remaining < 0) {
+                    $remaining = 0;
+                }
+                $response->headers->set('X-Ratelimit-Remaining', $remaining, false);
+            }
+        } catch (Exception $ex) {
+            Log::error($ex);
+        }
+
+        return $response;
+    }
 }
