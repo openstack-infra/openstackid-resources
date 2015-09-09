70b1dd4972b0e25495f13f24260657d48addb95c
Sebastian Marcet <smarcet@gmail.com>
2015-09-09 18:14:28 -0300

    Summit Application API
    WIP - Added Summit API
    
    Change-Id: I5494db9414809a3ca5e278919d084e895b2ed05a

---
+++ app/Models/summit/SummitVenue.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright 2015 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+namespace models\summit;
+
+/**
+ * Class SummitVenue
+ * @package models\summit
+ */
+class SummitVenue extends SummitGeoLocatedLocation
+{
+    protected $mtiClassType = 'abstract';
+
+    private $rooms = array();
+
+    public function addRoom(SummitVenueRoom $room)
+    {
+        array_push($this->rooms, $room);
+    }
+
+    public function toArray()
+    {
+        $values = parent::toArray();
+
+        $rooms = array();
+
+        foreach($this->rooms as $r)
+        {
+            array_push($rooms, $r->toArray());
+        }
+
+        if(count($rooms) > 0)
+            $values['rooms'] = $rooms;
+
+        return $values;
+    }
+
+    /**
+     * @return SummitVenueRoom[]
+     */
+    public function rooms()
+    {
+        return $this->hasMany('models\summit\SummitVenueRoom', 'VenueID', 'ID')->get();
+    }
+}
