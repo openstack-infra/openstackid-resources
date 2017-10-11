1527b197a9028ac46f683869b384d330a1f38f34
Sebastian Marcet <smarcet@gmail.com>
2017-10-11 12:54:03 -0300

    New current summit public endpoint
    added new current summit public endpoint
    
    Change-Id: I0c080719124ed41631f5ce6b37ef9dade809c8ba

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,6 +32,9 @@ Route::group([
     // summits
     Route::group(['prefix'=>'summits'], function() {
         Route::get('','OAuth2SummitApiController@getSummits');
+        Route::group(array('prefix' => '{id}'), function () {
+            Route::get('', [ 'middleware' => 'cache:'.Config::get('cache_api_response.get_summit_response_lifetime', 300), 'uses' => 'OAuth2SummitApiController@getSummit'])->where('id', 'current|[0-9]+');
+        });
         // locations
         Route::group(array('prefix' => 'locations'), function () {
             Route::group(array('prefix' => '{location_id}'), function () {
